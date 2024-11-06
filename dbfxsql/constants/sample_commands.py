d7c154571d8ab550efed4f25f420810b674e4b8c
Joel Abreu Rojas <jabreu@transmandu.com>
2024-11-06 10:26:30 -0400

    refactor(sample_commands): rename dbf/sql sources

--- dbfxsql/constants/sample_commands.py
+++ dbfxsql/constants/sample_commands.py
@@ -1,21 +1,21 @@
 DBF: dict[str, str] = {
-    "create": "dbfxsql create -s data.dbf -f id 'N(20,0)' -f name 'C(20)'",
-    "drop": "dbfxsql drop -s data.dbf",
-    "insert": "dbfxsql insert -s data.dbf -f id 1 -f name 'John Doe'",
-    "read": "dbfxsql read -s data.dbf -c id == 1",
-    "update": "dbfxsql update -s data.dbf -f name 'Jane Doe' -c id == 1",
-    "delete": "dbfxsql delete -s data.dbf -c id == 1",
+    "create": "dbfxsql create -s users.dbf -f id 'N(20,0)' -f name 'C(20)'",
+    "drop": "dbfxsql drop -s users.dbf",
+    "insert": "dbfxsql insert -s users.dbf -f id 1 -f name 'John Doe'",
+    "read": "dbfxsql read -s users.dbf -c id == 1",
+    "update": "dbfxsql update -s users.dbf -f name 'Jane Doe' -c id == 1",
+    "delete": "dbfxsql delete -s users.dbf -c id == 1",
     "migrate": "dbfxsql migrate -p SQL",
 }
 
 
 SQL: dict[str, str] = {
-    "create": "dbfxsql create -s data.sql -t users -f id 'integer primary key' -f name text",
-    "drop_database": "dbfxsql drop -s data.sql",
-    "drop_table": "dbfxsql drop -s data.sql -t users",
-    "insert": "dbfxsql insert -s data.sql -t users -f id 1 -f name 'John Doe'",
-    "read": "dbfxsql read -s data.sql -t users -c id == 1",
-    "update": "dbfxsql update -s data.sql -t users -f name 'Jane Doe' -c id == 1",
-    "delete": "dbfxsql delete -s data.sql -t users -c id == 1",
+    "create": "dbfxsql create -s company.sql -t users -f id 'integer primary key' -f name text",
+    "drop_database": "dbfxsql drop -s company.sql",
+    "drop_table": "dbfxsql drop -s company.sql -t users",
+    "insert": "dbfxsql insert -s company.sql -t users -f id 1 -f name 'John Doe'",
+    "read": "dbfxsql read -s company.sql -t users -c id == 1",
+    "update": "dbfxsql update -s company.sql -t users -f name 'Jane Doe' -c id == 1",
+    "delete": "dbfxsql delete -s company.sql -t users -c id == 1",
     "migrate": "dbfxsql migrate -p SQL",
 }
