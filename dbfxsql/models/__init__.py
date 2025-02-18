371cc3000bdb064f66a86f42488a87def2bbd186
Joel Abreu Rojas <joelabreurojas@gmail.com>
2025-02-18 10:43:39 -0400

    feat(models): expose data models in init file

---
+++ dbfxsql/models/__init__.py
@@ -0,0 +1,17 @@
+from .config import Config
+from .engine import Engine
+from .error_template import ErrorTemplate
+from .lazy_group import LazyGroup
+from .order_commands import OrderCommands
+from .relation import Relation
+from .sync_table import SyncTable
+
+__all__ = [
+    "Config",
+    "Engine",
+    "ErrorTemplate",
+    "LazyGroup",
+    "OrderCommands",
+    "Relation",
+    "SyncTable",
+]
