778a224c8dda66bdfa86bc3c69c12abc203914bf
Joel Abreu Rojas <joelabreurojas@gmail.com>
2025-02-22 09:55:26 -0400

    feat(engine): add default values and convert list

--- dbfxsql/models/engine.py
+++ dbfxsql/models/engine.py
@@ -1,10 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass(frozen=True, slots=True)
 class Engine:
     folderpaths: list[str]
     extensions: list[str]
-    db_server: str | None
-    db_user: str | None
-    db_password: str | None
+    db_server: str | None = None
+    db_user: str | None = None
+    db_password: str | None = None
+    convert_to_extension: list = field(default_factory=list)
