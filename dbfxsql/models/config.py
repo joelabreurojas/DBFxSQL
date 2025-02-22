503f00c72781534af89cd65292ed2a3bd76413c8
Joel Abreu Rojas <joelabreurojas@gmail.com>
2025-02-22 09:31:21 -0400

    refactor(config): update config model to dict format

--- dbfxsql/models/config.py
+++ dbfxsql/models/config.py
@@ -1,10 +1,7 @@
 from dataclasses import dataclass
 
-from .engine import Engine
-from .relation import Relation
-
 
 @dataclass(frozen=True, slots=True)
 class Config:
-    engines: list[Engine]
-    relations: list[Relation]
+    engines: dict[str, dict]
+    relations: list[dict]
