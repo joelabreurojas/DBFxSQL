86f386a6d07bba79128207580120570a47099f9a
Joel Abreu Rojas <joelabreurojas@gmail.com>
2025-02-25 15:14:14 -0400

    style(connection_errors): use backticks for source

--- dbfxsql/exceptions/connection_errors.py
+++ dbfxsql/exceptions/connection_errors.py
@@ -5,11 +5,11 @@ class DBFConnectionFailed(ErrorTemplate):
     """Error raised when a dBase database connection fails."""
 
     def __init__(self, source: str, error: Exception):
-        super().__init__(f"dBase connection with {source} failed.\n{error}")
+        super().__init__(f"dBase connection with `{source}` failed.\n{error}")
 
 
 class SQLConnectionFailed(ErrorTemplate):
     """Error raised when a SQL database connection fails."""
 
     def __init__(self, engine: str, source: str, error: Exception):
-        super().__init__(f"{engine} connection with {source} failed.\n{error}")
+        super().__init__(f"{engine} connection with `{source}` failed.\n{error}")
